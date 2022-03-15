f47a0915cbe5328a4443464ec48a52bd1a9b021a
Antonio Morga <anotniomc9719@gmail.com>
2022-03-15 19:06:02 +0100

    Created register controller and added it to module

--- src/Users/register/register.module.ts
+++ src/Users/register/register.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { userSchema } from 'src/models/user.model';
+import { RegisterController } from './register.controller';
+import { RegisterService } from './register.service';
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [
+        MongooseModule.forFeature([{ name: 'User', schema: userSchema }]),
+    ],
+    controllers: [RegisterController],
+    providers: [RegisterService],
 })
 export class RegisterModule {}
