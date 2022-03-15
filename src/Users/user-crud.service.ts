ca9ceffce621f8c45969cf3d0aca5653781e3662
Antonio Morga <anotniomc9719@gmail.com>
2022-03-15 18:56:36 +0100

    Fixed a problem with tests

--- src/Users/user-crud.service.ts
+++ src/Users/user-crud.service.ts
@@ -1,19 +1,9 @@
-import {
-    Injectable,
-    NotFoundException,
-    UnauthorizedException,
-} from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { compareSync } from 'bcryptjs';
-import { ifUser } from 'src/models/user.model';
+import { Injectable } from '@nestjs/common';
 import { CreateUserCrudDto } from './dto/create-user-crud.dto';
 import { UpdateUserCrudDto } from './dto/update-user-crud.dto';
 
 @Injectable()
 export class UserCrudService {
-    constructor(@InjectModel('User') private User: Model<ifUser>) {}
-
     create(createUserCrudDto: CreateUserCrudDto) {
         return 'This action adds a new userCrud';
     }
