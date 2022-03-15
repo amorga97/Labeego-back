177f82badb0f2fc9ba89315a082c77477a6c2040
Antonio Morga <anotniomc9719@gmail.com>
2022-03-15 14:08:45 +0100

    Created connection to atlas db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import LogsMiddleware from './utils/logs.middleware';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 @Module({
-    imports: [],
+    imports: [
+        ConfigModule.forRoot(),
+        MongooseModule.forRoot(
+            `mongodb+srv://${process.env.MONGO_USER}:${
+                process.env.PASSWORD
+            }@cluster0.pauk1.mongodb.net/${
+                process.env.NODE_ENV === 'test'
+                    ? process.env.TEST_DBNAME
+                    : process.env.DBNAME
+            }?retryWrites=true&w=majority`,
+        ),
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
