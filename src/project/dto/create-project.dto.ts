9489558d3ba2cc47361dc5fae454b2366754547c
Antonio Morga <anotniomc9719@gmail.com>
2022-03-16 19:27:31 +0100

    Created project dto

--- src/project/dto/create-project.dto.ts
+++ src/project/dto/create-project.dto.ts
@@ -1,1 +1,40 @@
-export class CreateProjectDto {}
+import {
+    IsArray,
+    IsDate,
+    IsObject,
+    IsString,
+    Length,
+    Matches,
+} from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateProjectDto {
+    @IsString()
+    @Length(5, 20)
+    title: string;
+
+    @IsString()
+    ref: string;
+
+    @IsObject()
+    user: mongoose.Types.ObjectId;
+
+    @IsObject()
+    client: mongoose.Types.ObjectId;
+
+    @IsString()
+    @Matches('to do')
+    @Matches('to review')
+    @Matches('doing')
+    @Matches('done')
+    status: string;
+
+    @IsDate()
+    appointment: Date;
+
+    @IsDate()
+    lastUpdate: Date;
+
+    @IsArray()
+    tasks: mongoose.Types.ObjectId[];
+}
