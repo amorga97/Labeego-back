7647501f69cda9a3771ee222de30dd7ee328df10
Antonio Morga <anotniomc9719@gmail.com>
2022-03-17 19:53:51 +0100

    Tested endpoints with postman and made some changes

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -3,7 +3,7 @@ import * as mongoose from 'mongoose';
 export const clientSchema = new mongoose.Schema({
     name: { type: String, required: true, minlength: 3, maxlength: 15 },
     phone: [{ type: String, minlength: 9, maxlength: 9 }],
-    adress: {
+    address: {
         street: { type: String, required: true, minlength: 5 },
         number: { type: Number, required: true },
     },
