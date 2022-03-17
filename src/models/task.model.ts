614209a3d1243f38530c9c178f821c3f67468ba9
Antonio Morga <anotniomc9719@gmail.com>
2022-03-17 15:20:36 +0100

    Modified CreateTaskDto and created partialTask interface

--- src/models/task.model.ts
+++ src/models/task.model.ts
@@ -1,15 +1,24 @@
 import * as mongoose from 'mongoose';
 
 export const taskSchema = new mongoose.Schema({
-    title: { type: String, required: true, minlength: 5, maxlength: 20 },
+    title: { type: String, required: true, minlength: 5, maxlength: 50 },
     description: { type: String, required: false },
     project: { type: mongoose.SchemaTypes.ObjectId, required: true },
     status: { type: String, required: true },
 });
 
 export interface ifTask {
+    _id?: mongoose.Types.ObjectId;
     title: string;
     description: string;
     project: mongoose.Types.ObjectId;
     status: string;
+}
+
+export interface ifPartialTask {
+    _id?: mongoose.Types.ObjectId;
+    title?: string;
+    description?: string;
+    project?: mongoose.Types.ObjectId;
+    status?: string;
 }
--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,1 +1,11 @@
-export class CreateTaskDto {}
+import { IsObject, IsString, Length } from 'class-validator';
+import mongoose from 'mongoose';
+
+export class CreateTaskDto {
+    @IsString()
+    @Length(5, 50)
+    title: string;
+
+    @IsObject()
+    project: mongoose.Types.ObjectId;
+}
