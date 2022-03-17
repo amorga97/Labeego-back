feab5ad37066e280e455364facfa73da153decd8
Antonio Morga <anotniomc9719@gmail.com>
2022-03-17 16:35:59 +0100

    Created test for task.controller

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,11 +1,7 @@
-import { IsObject, IsString, Length } from 'class-validator';
-import mongoose from 'mongoose';
+import { IsString, Length } from 'class-validator';
 
 export class CreateTaskDto {
     @IsString()
     @Length(5, 50)
     title: string;
-
-    @IsObject()
-    project: mongoose.Types.ObjectId;
 }
--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,20 +1,78 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { UpdateProjectDto } from 'src/project/dto/update-project.dto';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { TasksController } from './tasks.controller';
 import { TasksService } from './tasks.service';
 
 describe('TasksController', () => {
-  let controller: TasksController;
+    const mockTaskToAdd = {
+        title: "antonio's project",
+    };
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [TasksController],
-      providers: [TasksService],
-    }).compile();
+    let controller: TasksController;
+    let service: TasksService;
 
-    controller = module.get<TasksController>(TasksController);
-  });
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [TasksController],
+            providers: [
+                {
+                    provide: TasksService,
+                    useValue: {
+                        create: jest.fn(),
+                        findAll: jest.fn(),
+                        findOne: jest.fn(),
+                        update: jest.fn(),
+                        remove: jest.fn(),
+                    },
+                },
+            ],
+        }).compile();
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+        controller = module.get<TasksController>(TasksController);
+        service = module.get<TasksService>(TasksService);
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('When calling controller.create', () => {
+        test('Then service.create should be called', async () => {
+            await controller.create('', mockTaskToAdd as CreateTaskDto);
+            expect(service.create).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.findall', () => {
+        test('Then service.findAll should be called', async () => {
+            await controller.findAll('Bearer token');
+            expect(service.findAll).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.findOne', () => {
+        test('Then service.findOne should be called', async () => {
+            await controller.findOne('id', 'Bearer token');
+            expect(service.findOne).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.update', () => {
+        test('Then service.update should be called', async () => {
+            await controller.update(
+                'projectId',
+                'taskId',
+                mockTaskToAdd as UpdateProjectDto,
+            );
+            expect(service.update).toHaveBeenCalled();
+        });
+    });
+
+    describe('When calling controller.remove', () => {
+        test('Then service.findOne should be called', async () => {
+            await controller.remove('Projectid', 'TaskID');
+            expect(service.remove).toHaveBeenCalled();
+        });
+    });
 });
