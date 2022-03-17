e5e51d25a86fbc2f64299bdde2b97d7bfe7dee6c
Antonio Morga <anotniomc9719@gmail.com>
2022-03-17 18:07:17 +0100

    Generated basic crud files for clients

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,7 @@ import { LoginModule } from './login/login.module';
 import { RegisterModule } from './register/register.module';
 import { ProjectModule } from './project/project.module';
 import { TasksModule } from './tasks/tasks.module';
+import { ClientsModule } from './clients/clients.module';
 @Module({
     imports: [
         ConfigModule.forRoot(),
@@ -26,6 +27,7 @@ import { TasksModule } from './tasks/tasks.module';
         RegisterModule,
         ProjectModule,
         TasksModule,
+        ClientsModule,
     ],
     controllers: [AppController],
     providers: [AppService],
