8008988096e4c90e415913c1c6349786514936ea
Antonio Morga <anotniomc9719@gmail.com>
2022-03-17 13:24:22 +0100

    Fixed test for register.service to adapt to some changes

--- src/register/register.service.spec.ts
+++ src/register/register.service.spec.ts
@@ -18,6 +18,7 @@ describe('Given RegisterService', () => {
 
     const mockRepository = {
         create: jest.fn().mockResolvedValue(mockUserToAdd),
+        findByIdAndUpdate: jest.fn().mockResolvedValue(mockUserToAdd),
     };
 
     beforeEach(async () => {
@@ -52,7 +53,7 @@ describe('Given RegisterService', () => {
 
     describe('When calling it with invalid or missing params', () => {
         test('Then it should throw an exception', () => {
-            mockRepository.create.mockImplementation(async () => {
+            mockRepository.findByIdAndUpdate.mockImplementation(async () => {
                 throw new ForbiddenException();
             });
             expect(
